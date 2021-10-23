d1231b598b06e96686d61cac91a2ab0c7acbff3f
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-10-23 17:36:15 -0500

    Amount and buy now

--- lib/src/pages/shoe_desc_page.dart
+++ lib/src/pages/shoe_desc_page.dart
@@ -1,5 +1,6 @@
 import 'package:app_shoes/src/widgets/shoe_description.dart';
 import 'package:app_shoes/src/widgets/shoe_preview.dart';
+import 'package:app_shoes/src/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class ShoeDescPage extends StatelessWidget {
@@ -16,12 +17,32 @@ class ShoeDescPage extends StatelessWidget {
               child: Column(
                 children: const [
                   ShoeDescription(title: 'Nike Air Max 720', description: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so."),
+                  _BuyNow()
                 ],
               ),
             )
           )
         ],
       ),
+    );
+  }
+}
+
+class _BuyNow extends StatelessWidget {
+  const _BuyNow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        margin: const EdgeInsets.only(top: 20.0),
+        child: Row( 
+          children:const [
+              Text("\$180.0", style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)),
+              Spacer(),
+              OrangeButton(texto: 'Buy now', alto: 40.0, ancho: 100.0,)
+          ],
+        ),
     );
   }
 }
--- lib/src/widgets/orange_button.dart
+++ lib/src/widgets/orange_button.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 
 class OrangeButton extends StatelessWidget {
   final String texto;
-  const OrangeButton({Key? key, required this.texto}) : super(key: key);
+  final double? alto;
+  final double? ancho;
+
+  const OrangeButton({Key? key, required this.texto, this.alto = 50.0, this.ancho = 150.0}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      width: 150.0,
-      height: 50.0,
+      width: ancho!,
+      height: alto!,
       decoration: BoxDecoration(
         color: Colors.orange,
         borderRadius: BorderRadius.circular(100.0)
