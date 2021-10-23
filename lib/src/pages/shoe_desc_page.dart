c5e31452a2cea121730c51412b422ad5b40336a6
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-10-23 18:31:05 -0500

    Circular color selection buttons

--- lib/src/pages/shoe_desc_page.dart
+++ lib/src/pages/shoe_desc_page.dart
@@ -17,12 +17,58 @@ class ShoeDescPage extends StatelessWidget {
               child: Column(
                 children: const [
                   ShoeDescription(title: 'Nike Air Max 720', description: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so."),
-                  _BuyNow()
+                  _BuyNow(),
+                  _ColorsSection()
                 ],
               ),
             )
           )
         ],
+      ),
+    );
+  }
+}
+
+class _ColorsSection extends StatelessWidget {
+  const _ColorsSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10.0),
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Stack(
+              children: const [
+              Positioned(left: 90.0, child: _ButtonColor(color: Color(0xffC6D642))),
+              Positioned(left: 60.0, child: _ButtonColor(color: Color(0xffFFAD29))),
+              Positioned(left: 30.0, child: _ButtonColor(color: Color(0xff2099F1))),
+              _ButtonColor(color: Color(0xff364D56)), 
+              ],
+            ),
+          ),
+          const Spacer(),
+          const OrangeButton(texto: "More related items", alto: 30.0, ancho: 170.0, color: Color(0xffFFC675),)
+        ],
+      ),
+    );
+  }
+}
+
+class _ButtonColor extends StatelessWidget {
+  final Color color;
+  const _ButtonColor({Key? key, required this.color }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45.0,
+      width: 45.0,
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle
       ),
     );
   }
--- lib/src/widgets/orange_button.dart
+++ lib/src/widgets/orange_button.dart
@@ -4,8 +4,9 @@ class OrangeButton extends StatelessWidget {
   final String texto;
   final double? alto;
   final double? ancho;
+  final Color color;
 
-  const OrangeButton({Key? key, required this.texto, this.alto = 50.0, this.ancho = 150.0}) : super(key: key);
+  const OrangeButton({Key? key, required this.texto, this.alto = 50.0, this.ancho = 150.0, this.color = Colors.orange}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,7 @@ class OrangeButton extends StatelessWidget {
       width: ancho!,
       height: alto!,
       decoration: BoxDecoration(
-        color: Colors.orange,
+        color: color,
         borderRadius: BorderRadius.circular(100.0)
       ),
       child: Text(texto, style: const TextStyle(color: Colors.white)),
