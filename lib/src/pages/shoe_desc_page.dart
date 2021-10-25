da919ddc8cf01d994c4b3e3cd69849eeffb48c8f
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-10-24 19:28:46 -0500

    Final options

--- lib/src/pages/shoe_desc_page.dart
+++ lib/src/pages/shoe_desc_page.dart
@@ -11,20 +11,76 @@ class ShoeDescPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          const ShoePreview(fullScreen: true),
+          Stack(
+            children: [
+              const ShoePreview(fullScreen: true),
+              Positioned(
+                top: 50.0,
+                child: FloatingActionButton(
+                  onPressed:()=>{} , 
+                  child: const Icon(Icons.chevron_left, color: Colors.white, size: 60.0),
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  highlightElevation: 0,
+                )
+              )
+            ],
+          ),
           Expanded(
             child: SingleChildScrollView(
               child: Column(
                 children: const [
                   ShoeDescription(title: 'Nike Air Max 720', description: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so."),
                   _BuyNow(),
-                  _ColorsSection()
+                  _ColorsSection(),
+                  _LikeCartSetting()
                 ],
               ),
             )
           )
         ],
       ),
+    );
+  }
+}
+
+class _LikeCartSetting extends StatelessWidget {
+  const _LikeCartSetting({Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 30.0),
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const _ShadedButton(icon: Icon(Icons.favorite, color: Colors.red, size: 25.0,)),
+          _ShadedButton(icon: Icon(Icons.shopping_cart, color: Colors.grey.withOpacity(0.4), size: 25.0,)),
+          _ShadedButton(icon: Icon(Icons.settings, color: Colors.grey.withOpacity(0.4), size: 25.0,)),
+        ],
+      ),
+    );
+  }
+}
+
+class _ShadedButton extends StatelessWidget {
+  final Icon icon;
+  const _ShadedButton({Key? key, required this.icon}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      width: 50.0,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(color: Colors.black12, spreadRadius: -5, blurRadius: 20, offset: Offset(0, 10))
+        ]
+      ),
+      child: icon,
     );
   }
 }
